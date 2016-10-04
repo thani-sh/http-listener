03e9f88b1f8f5728cbf0ddd3fb919ec5f5e4ebae
Muhammed Thanish <mnmtanish@users.noreply.github.com>
2016-10-04 17:39:47 +0530

    Fix syntax error

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
-var http = require('http')
+#!/usr/bin/env node
 
-var server = http.createServer(function (req, res) {
+const http = require('http')
+const port = process.env.PORT || 3000
+
+const server = http.createServer(function (req, res) {
   console.log(`\n${new Date().toLocaleTimeString()}`)
   console.log(`${req.method} ${req.url}`)
-  console.log(`HEADERS: ${JSON.stringify(req.headers, null, 2)}`);
+  console.log(`HEADERS: ${JSON.stringify(req.headers, null, 2)}`)
   res.writeHead(200, {'Content-Type': 'text/plain'})
   res.end('Hello World\n')
 })
 
-server.listen(process.env.PORT)
+server.listen(port)
