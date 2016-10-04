9f55dc6c1f48e0168772faccd228c6847187aac0
Muhammed Thanish <mnmtanish@users.noreply.github.com>
2016-10-04 17:49:42 +0530

    Log request body

--- index.js
+++ index.js
@@ -7,8 +7,14 @@ const server = http.createServer(function (req, res) {
   console.log(`\n${new Date().toLocaleTimeString()}`)
   console.log(`${req.method} ${req.url}`)
   console.log(`HEADERS: ${JSON.stringify(req.headers, null, 2)}`)
-  res.writeHead(200, {'Content-Type': 'text/plain'})
-  res.end('Hello World\n')
+
+  let data = '';
+  req.on('data', d => data = data + d.toString())
+  req.on('end', () => {
+    console.log(`CONTENT: ${data}`)
+    res.writeHead(200, {'Content-Type': 'text/plain'})
+    res.end('Hello World\n')
+  })
 })
 
 server.listen(port)
